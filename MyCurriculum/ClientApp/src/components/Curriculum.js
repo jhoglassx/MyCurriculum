d3ebecd736d1740b48fb2aa81d56c0a86f84ff9d
Jhoglas <jhoglassx@gmail.com>
2021-11-11 22:22:20 -0300

    Create Compoment Curriculum

---
+++ MyCurriculum/ClientApp/src/components/Curriculum.js
@@ -0,0 +1,45 @@
+﻿import React, { Compoment } from 'react'
+import { Link } from 'react-router-dom'
+
+export class Curriculum extends Compoment {
+    static displayName = "Curriculo";
+
+    constructor() {
+        super();
+
+        this.state = {curriculum:[],loading: true }
+    }
+
+    compomentDidMount() {
+
+        this.loadCuriculum();
+
+    }
+
+    static handleEdit(id) {
+        window.location.href = "/Curriculums/edit" + id;
+    }
+
+    static handleDelete(id) {
+        if (!window.confirm("Vocêdeseja deletar o Curriculum : " + id)) {
+            return;
+        }
+        else {
+            fetch('api/Curriculums/' + id, { method: 'delete' })
+            .then(json => {
+                window.location.hre = "Curriculum";
+                alert('Deletado com Sucesso');
+            })
+        } 
+    }
+
+    async loadCuriculum() {
+        const response = await fetch('api/Curriculums');
+        const date = await response.json();
+        this.setState({ corriculum: date, loading:false });
+    }
+
+    render() {
+
+    }
+}
