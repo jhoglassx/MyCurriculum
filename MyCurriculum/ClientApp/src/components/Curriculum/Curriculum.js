b632856668424bfbdd62b10b6d74fb22c73f3096
Jhoglas <jhoglassx@gmail.com>
2021-11-16 16:56:14 -0300

    react

---
+++ MyCurriculum/ClientApp/src/components/Curriculum/Curriculum.js
@@ -0,0 +1,30 @@
+﻿import { data } from 'jquery';
+import React, { Component } from 'react'
+//import './components/Curriculum/Curriculum.css'
+
+export class Curriculum extends Component {
+    static displayName = "Curriculo";
+
+    constructor() {
+        super();
+        this.state = { curriculums: Object, loading: true }
+    }
+
+    async loadCurriculum() {
+        const response = await fetch('api/Curriculums/1');
+        const data = await response.json;
+        this.setState({ curriculums: data, loading: false });
+    }
+
+    componentDidMount() {
+        this.loadCurriculum();
+    }
+
+    render() {
+        return (
+            <div className="curiculum">
+                <span>{this.state.curriculums}</span>
+            </div>
+        );
+    }
+}
